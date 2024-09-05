941534a58bfa214164ef14d6a4b4399e9b0630fb
mohammed hassan <mohammedhassanage26@gmail.com>
2024-09-05 11:55:08 +0100

    first commit3

--- src/componant/tesimonial/Testimonial.jsx
+++ src/componant/tesimonial/Testimonial.jsx
@@ -208,8 +208,8 @@ const Testimonial = () => {
               <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
                 <img
                   className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                  src={testimonialOne}
-                  alt="testimonialOne"
+                  src={testimonialTwo}
+                  alt="testimonialTwo"
                 />
                 <div className="w-full flex flex-col justify-end">
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
